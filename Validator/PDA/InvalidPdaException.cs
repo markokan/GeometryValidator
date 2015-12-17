e833a54e79d8f887a320800d167ad93c15b53aae
markokan <markokan@sci.fi>
2015-12-17 23:43:10 +0200

    init

---
+++ Validator/PDA/InvalidPdaException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorUtil.PDA
+{
+    public class InvalidPdaException : ApplicationException
+    {
+        public InvalidPdaException() : base()
+        { }
+
+        public InvalidPdaException(string message) : base(message)
+        { }
+
+        public InvalidPdaException(string message, Exception inner) : base(message, inner)
+        { }
+
+        public InvalidPdaException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext ctx) : base(info, ctx)
+        { }
+    }
+}
